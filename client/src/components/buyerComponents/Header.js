50f10686fe1293e4b5b8cc4d6fb80b782b678933
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-04-11 08:49:29 +0530

    Update Header.js

--- client/src/components/buyerComponents/Header.js
+++ client/src/components/buyerComponents/Header.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { AppBar, Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { AppBar, Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme, Button } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SpaIcon from '@mui/icons-material/Spa';
 import DrawerComp from "./Drawer";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useLogout } from "../../hooks/useLogout";
 
 const Header = () => {
+    const { user } = useAuthContext();
     const [value, setValue] = useState(0);
     const theme = useTheme();
     const isMatch = useMediaQuery(theme.breakpoints.down("md"));
 
+    const {logout} = useLogout();
 
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+
+    const handleLogoutClick =()=>{
+        logout()
+    }
     const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenUserMenu = (event) => {
@@ -26,7 +34,9 @@ const Header = () => {
         <>
             <AppBar sx={{ background: "#063970", padding: "15px" }}>
                 <Toolbar>
-                    <SpaIcon sx={{ transform: "scale(2)" }} />
+                    <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                        <SpaIcon sx={{ transform: "scale(2)" }} />
+                    </Link>
                     <Typography sx={{
                         fontSize: "2rem",
                         paddingLeft: "2%",
@@ -36,14 +46,13 @@ const Header = () => {
                         letterSpacing: '.3rem',
                         color: 'inherit',
                         textDecoration: 'none',
-                    }}>
+                    }} component={Link} to="/" >
                         AYURMART
                     </Typography>
                     {isMatch ? (
                         <>
-
                             <Tooltip title="Open Cart">
-                                <Link to="/cart" style={{ marginLeft: "auto",textDecoration:"none", color:"white"}}>
+                                <Link to="/cart" style={{ marginLeft: "auto", textDecoration: "none", color: "white" }}>
                                     <ShoppingCartIcon sx={{ transform: "scale(1.5)" }} />
                                 </Link>
                             </Tooltip>
@@ -69,11 +78,12 @@ const Header = () => {
                                     open={Boolean(anchorElUser)}
                                     onClose={handleCloseUserMenu}
                                 >
-                                    {settings.map((setting) => (
-                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                            <Typography textAlign="center">{setting}</Typography>
-                                        </MenuItem>
-                                    ))}
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center" sx={{ textDecoration: "none", color: "black" }} component={Link} to="/user-profile">Profile</Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleLogoutClick}>
+                                        <Typography textAlign="center" sx={{ textDecoration: "none", color: "black" }} component={Link} to="/">Logout</Typography>
+                                    </MenuItem>
                                 </Menu>
                             </Box>
                             <DrawerComp />
@@ -87,46 +97,59 @@ const Header = () => {
                                 value={value}
                                 onChange={(e, value) => setValue(value)}
                             >
-                                <Tab label="Home" />
-                                <Tab label="Category" />
-                                <Tab label="About Us" />
-                                <Tab label="Contact" />
+                                <Tab label="Home" LinkComponent={Link} to="/" />
+                                <Tab label="About Us" LinkComponent={Link} to="/about" />
+                                <Tab label="Contact" LinkComponent={Link} to="/contact" />
                             </Tabs>
 
-                            <Tooltip title="Open Cart">
-                                <Link to="/cart" style={{marginLeft: "auto",textDecoration:"none", color:"white"}}>
-                                    <ShoppingCartIcon sx={{ transform: "scale(1.5)" }} />
-                                </Link>
-                            </Tooltip>
-                            <Box sx={{ flexGrow: 0, marginLeft: "40px" }}>
-                                <Tooltip title="Open settings">
-                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                    </IconButton>
-                                </Tooltip>
-                                <Menu
-                                    sx={{ mt: '45px' }}
-                                    id="menu-appbar"
-                                    anchorEl={anchorElUser}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    open={Boolean(anchorElUser)}
-                                    onClose={handleCloseUserMenu}
-                                >
-                                    {settings.map((setting) => (
-                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                            <Typography textAlign="center">{setting}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
+                            {user ? (
+                                <>
+                                    <Tooltip title="Open Cart">
+                                        <Link to="/cart" style={{ marginLeft: "auto", textDecoration: "none", color: "white" }}>
+                                            <ShoppingCartIcon sx={{ transform: "scale(1.5)" }} />
+                                        </Link>
+                                    </Tooltip>
+                                    <Box sx={{ flexGrow: 0, marginLeft: "40px" }}>
+                                        <Tooltip title="Open settings">
+                                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Menu
+                                            sx={{ mt: '45px' }}
+                                            id="menu-appbar"
+                                            anchorEl={anchorElUser}
+                                            anchorOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'right',
+                                            }}
+                                            keepMounted
+                                            transformOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'right',
+                                            }}
+                                            open={Boolean(anchorElUser)}
+                                            onClose={handleCloseUserMenu}
+                                        >
+
+                                            <MenuItem onClick={handleCloseUserMenu}>
+                                                <Typography textAlign="center" sx={{ textDecoration: "none", color: "black" }} component={Link} to="/user-profile">Profile</Typography>
+                                            </MenuItem>
+                                            <MenuItem onClick={handleLogoutClick}>
+                                                <Typography textAlign="center" sx={{ textDecoration: "none", color: "black" }} component={Link} to="/">Logout</Typography>
+                                            </MenuItem>
+
+                                        </Menu>
+                                    </Box>
+                                </>
+                            ) : (
+                                <>
+                                    <Box sx={{ flexGrow: 0, marginLeft: "40px" }}>
+                                        <Button LinkComponent={Link} to="/login">Login</Button>
+                                        <Button LinkComponent={Link} to="/signup">Register</Button>
+                                    </Box>
+                                </>
+                            )}
                         </>
                     )}
                 </Toolbar>
