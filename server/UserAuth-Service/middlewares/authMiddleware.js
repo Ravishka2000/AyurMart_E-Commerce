f8e6a6919e08e751a47d6fa841e240c3a04c3a35
IT21023132 <peirissemini@gmail.com>
2023-04-27 21:18:35 +0530

    put comments on userauth controllers

--- server/UserAuth-Service/middlewares/authMiddleware.js
+++ server/UserAuth-Service/middlewares/authMiddleware.js
@@ -2,12 +2,15 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 
+
 const authMiddleware = asyncHandler (async (req, res, next) => {
     let token;
+    //get the token from auhtorization header 
     if(req?.headers?.authorization?.startsWith("Bearer")){
         token = req.headers?.authorization?.split(" ")[1];
         try {
             if(token){
+                //decode the token using jwt 
                 const decoded = jwt.verify(token, process.env.JWT_SECRET);
                 console.log(decoded)
                 const user = await User.findById(decoded?.id);
@@ -15,10 +18,12 @@ const authMiddleware = asyncHandler (async (req, res, next) => {
                 next();
             }
         } catch (error) {
+            //handle errors is token is expired
             console.error(error)
             throw new Error("No authorized token expired, please try again");
         }
     }else{
+        //if token is not attached to the header
         throw new Error("There is no Token attached to Header");
     }
 });
