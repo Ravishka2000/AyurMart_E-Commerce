54c518c843e56d91f29b318c813b635c624565b5
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-04-27 19:17:19 +0530

    delete previous project

--- server/config/refreshToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateRefreshToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "3d"});
-};
-
-export default generateRefreshToken;
